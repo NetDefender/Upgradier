d78b5ad278eb6fbcd523ebdf8be0cd6653094cb8
NetDefender <danielsith@hotmail.com>
2023-07-31 23:26:36 +0200

    feat: testcontainers gh

--- tests/Upgradier.Tests/SqlServer/SqlFactory_Tests.cs
+++ tests/Upgradier.Tests/SqlServer/SqlFactory_Tests.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit.Abstractions;
 
 namespace Upgradier.Tests.SqlServer;
 
-public sealed class SqlFactory_Tests
+public sealed class SqlFactory_Tests : IClassFixture<SqlServerDatabaseFixture>
 {
+    private readonly string _connectionString;
+    private readonly ITestOutputHelper _output;
+
+    public SqlFactory_Tests(ITestOutputHelper output, SqlServerDatabaseFixture fixture)
+    {
+        _connectionString = fixture.ConnectionString!;
+        _output = output;
+    }
+
+    [Fact]
+    public async Task Connectionstring_is_not_null()
+    {
+        _output.WriteLine(_connectionString);
+        Assert.NotNull(_connectionString);
+    }
 }
