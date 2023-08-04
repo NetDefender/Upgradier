718dd90dafe8a8a57cb55a04a1103c9517e8cbb6
NetDefender <danielsith@hotmail.com>
2023-08-04 22:49:15 +0200

    tests: WebScriptStrategy 2/n

--- tests/Upgradier.Tests/Core/FileScriptStrategy.Tests.cs
+++ tests/Upgradier.Tests/Core/FileScriptStrategy.Tests.cs
@@ -8,7 +8,7 @@ public sealed class FileScriptStrategy_Tests
     public async Task GetAllScripts_Works_When_Base_Directory_Not_Ends_With_DirectorySeparator()
     {
         using CancellationTokenSource cancellationTokenSource = new();
-        FileScriptStrategy strategy = new("Core/Scripts", "SqlServer", null);
+        FileScriptStrategy strategy = new($"Core{Path.DirectorySeparatorChar}Scripts", "SqlServer", null);
         IEnumerable<Script> scripts = await strategy.GetScriptsAsync(cancellationTokenSource.Token).ConfigureAwait(false);
         Assert.NotNull(scripts.FirstOrDefault(s => s.VersionId == 1));
         Assert.NotNull(scripts.FirstOrDefault(s => s.VersionId == 2));
@@ -19,7 +19,7 @@ public sealed class FileScriptStrategy_Tests
     public async Task GetAllScripts_Works_When_Base_Directory_Ends_With_DirectorySeparator()
     {
         using CancellationTokenSource cancellationTokenSource = new();
-        FileScriptStrategy strategy = new("Core/Scripts/", "SqlServer", null);
+        FileScriptStrategy strategy = new($"Core{Path.DirectorySeparatorChar}Scripts{Path.DirectorySeparatorChar}", "SqlServer", null);
         IEnumerable<Script> scripts = await strategy.GetScriptsAsync(cancellationTokenSource.Token).ConfigureAwait(false);
         Assert.NotNull(scripts.FirstOrDefault(s => s.VersionId == 1));
         Assert.NotNull(scripts.FirstOrDefault(s => s.VersionId == 2));
@@ -30,7 +30,7 @@ public sealed class FileScriptStrategy_Tests
     public async Task GetAllScripts_Returns_2_Scripts_In_Null_Environment()
     {
         using CancellationTokenSource cancellationTokenSource = new();
-        FileScriptStrategy strategy = new("Core/Scripts", "SqlServer", null);
+        FileScriptStrategy strategy = new($"Core{Path.DirectorySeparatorChar}Scripts", "SqlServer", null);
         IEnumerable<Script> scripts = await strategy.GetScriptsAsync(cancellationTokenSource.Token).ConfigureAwait(false);
         Assert.NotNull(scripts.FirstOrDefault(s => s.VersionId == 1));
         Assert.NotNull(scripts.FirstOrDefault(s => s.VersionId == 2));
@@ -41,7 +41,7 @@ public sealed class FileScriptStrategy_Tests
     public async Task GetAllScripts_Returns_3_Scripts_In_Dev_Environment()
     {
         using CancellationTokenSource cancellationTokenSource = new();
-        FileScriptStrategy strategy = new("Core/Scripts", "SqlServer", "Dev");
+        FileScriptStrategy strategy = new($"Core{Path.DirectorySeparatorChar}Scripts", "SqlServer", "Dev");
         IEnumerable<Script> scripts = await strategy.GetScriptsAsync(cancellationTokenSource.Token).ConfigureAwait(false);
         Assert.NotNull(scripts.FirstOrDefault(s => s.VersionId == 1));
         Assert.NotNull(scripts.FirstOrDefault(s => s.VersionId == 2));
@@ -50,7 +50,7 @@ public sealed class FileScriptStrategy_Tests
     }
 
     [Fact]
-    public void FileStrategy_Throws_If_BaseDirectory_Is_Null_Or_Empty()
+    public void Ctor_Throws_If_BaseDirectory_Is_Null_Or_Empty()
     {
         Assert.Throws<ArgumentNullException>("baseDirectory", () =>
         {
@@ -63,12 +63,28 @@ public sealed class FileScriptStrategy_Tests
     }
 
     [Fact]
-    public async Task FileStrategy_Throws_If_File_Index_Json_Not_Exists()
+    public async Task Ctor_Throws_If_File_Index_Json_Not_Exists()
     {
         await Assert.ThrowsAsync<DirectoryNotFoundException>(async () =>
         {
-            FileScriptStrategy strategy = new("NonExistentDirectory/Scripts", "SqlServer", "Dev");
+            FileScriptStrategy strategy = new($"NonExistentDirectory{Path.DirectorySeparatorChar}Scripts", "SqlServer", "Dev");
             IEnumerable<Script> scripts = await strategy.GetScriptsAsync(CancellationToken.None).ConfigureAwait(false);
         });
+    }
+
+    [Theory]
+    [InlineData(1, "SqlServer", null)]
+    [InlineData(2, "SqlServer", null)]
+    [InlineData(1, "SqlServer", "Dev")]
+    [InlineData(2, "SqlServer", "Dev")]
+    [InlineData(3, "SqlServer", "Dev")]
+    public async Task GetScriptContentsAsync_Get_Contents_By_VersionId(int versionId, string provider, string? environment)
+    {
+        string expectedScriptContents = await File.ReadAllTextAsync(Path.Combine("Core", "Scripts", provider, environment ?? string.Empty, $"{versionId}.sql")).ConfigureAwait(false);
+        FileScriptStrategy strategy = new($"Core{Path.DirectorySeparatorChar}Scripts", provider, environment);
+        using StreamReader actualStreamContent = await strategy.GetScriptContentsAsync(new Script { VersionId = versionId }, CancellationToken.None).ConfigureAwait(false);
+        Assert.NotNull(actualStreamContent);
+        string actualScriptContents = await actualStreamContent.ReadToEndAsync(CancellationToken.None).ConfigureAwait(false);
+        Assert.Equal(expectedScriptContents, actualScriptContents);
     }
 }
--- tests/Upgradier.Tests/Core/WebScriptStrategy.Tests.cs
+++ tests/Upgradier.Tests/Core/WebScriptStrategy.Tests.cs
@@ -22,4 +22,21 @@ public class WebScriptStrategyTests : IClassFixture<ScriptServerFixture>
         Assert.NotNull(scripts.FirstOrDefault(s => s.VersionId == 2));
         Assert.Equal(2, scripts.Count());
     }
+
+    [Theory]
+    [InlineData(1, "SqlServer", null)]
+    [InlineData(2, "SqlServer", null)]
+    [InlineData(1, "SqlServer", "Dev")]
+    [InlineData(2, "SqlServer", "Dev")]
+    [InlineData(3, "SqlServer", "Dev")]
+    public async Task GetScriptContentsAsync_Get_Contents_By_VersionId(int versionId, string provider, string? environment)
+    {
+        Assert.NotNull(_scriptServerfixture.ScriptsUri);
+        string expectedScriptContents = await File.ReadAllTextAsync(Path.Combine("Core", "Scripts", provider, environment ?? string.Empty, $"{versionId}.sql")).ConfigureAwait(false);
+        WebScriptStrategy strategy = new(_scriptServerfixture.ScriptsUri, provider, environment);
+        using StreamReader actualStreamContent = await strategy.GetScriptContentsAsync(new Script { VersionId = versionId }, CancellationToken.None).ConfigureAwait(false);
+        Assert.NotNull(actualStreamContent);
+        string actualScriptContents = await actualStreamContent.ReadToEndAsync(CancellationToken.None).ConfigureAwait(false);
+        Assert.Equal(expectedScriptContents, actualScriptContents);
+    }
 }
