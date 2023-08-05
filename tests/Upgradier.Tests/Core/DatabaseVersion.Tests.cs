32249b59256ccd9d403138edfb8d7f9a3d367dd2
NetDefender <danielsith@hotmail.com>
2023-08-05 22:39:05 +0200

    tests: DatabaseVersion

---
+++ tests/Upgradier.Tests/Core/DatabaseVersion.Tests.cs
@@ -0,0 +1,23 @@
+﻿using Upgradier.Core;
+
+namespace Upgradier.Tests.Core
+{
+    public sealed class DatabaseVersion_Tests
+    {
+        [Fact]
+        public void VersionId_Is_Settable_And_Accesible()
+        {
+            DatabaseVersion version = new()
+            { VersionId = 1 };
+            Assert.Equal(1, version.VersionId);
+        }
+
+        [Fact]
+        public void VersionId_Can_Be_Negative()
+        {
+            DatabaseVersion version = new()
+            { VersionId = -1 };
+            Assert.Equal(-1, version.VersionId);
+        }
+    }
+}
