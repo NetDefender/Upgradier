840ecc1d0d9f2295e71ff38cd4566d6369cc00cf
NetDefender <danielsith@hotmail.com>
2023-08-05 22:33:53 +0200

    test: Source_Tests + solution items

--- src/Core/Source.cs
+++ src/Core/Source.cs
@@ -4,6 +4,9 @@ public class Source
 {
     public Source(string name, string provider, string connectionString)
     {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentException.ThrowIfNullOrEmpty(provider);
+        ArgumentException.ThrowIfNullOrEmpty(connectionString);
         Name = name;
         Provider = provider;
         ConnectionString = connectionString;
---
+++ tests/Upgradier.Tests/Core/Source.Tests.cs
@@ -0,0 +1,40 @@
+﻿using Upgradier.Core;
+using Xunit.Abstractions;
+
+namespace Upgradier.Tests.Core;
+
+public sealed class Source_Tests
+{
+    private readonly ITestOutputHelper _output;
+
+    public Source_Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Theory]
+    [InlineData("NotImportant", "NotImportant", null)]
+    [InlineData("NotImportant", null, "NotImportant")]
+    [InlineData("NotImportant", null, null)]
+    [InlineData(null, "NotImportant", "NotImportant")]
+    [InlineData(null, "NotImportant", null)]
+    [InlineData(null, null, "NotImportant")]
+    [InlineData(null, null, null)]
+    public void If_Any_Property_Is_Null_Throws(string? name, string? provider, string? connectionString)
+    {
+        Assert.Throws<ArgumentNullException>(() => new Source(name, provider, connectionString));
+    }
+
+    [Theory]
+    [InlineData("", "", "")]
+    [InlineData("", "", "NotImportant")]
+    [InlineData("", "NotImportant", "")]
+    [InlineData("", "NotImportant", "NotImportant")]
+    [InlineData("NotImportant", "", "")]
+    [InlineData("NotImportant", "", "NotImportant")]
+    [InlineData("NotImportant", "NotImportant", "")]
+    public void If_Any_Property_Is_Empty_Throws(string name, string provider, string connectionString)
+    {
+        Assert.Throws<ArgumentException>(() => new Source(name, provider, connectionString));
+    }
+}
