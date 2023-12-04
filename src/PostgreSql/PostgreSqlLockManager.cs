61d9bbcf26cc14142483011e95422e3180aa3b3a
NetDefender <danielsith@hotmail.com>
2023-12-04 01:24:39 +0100

    fix: Postgresql locks

--- src/PostgreSql/PostgreSqlLockManager.cs
+++ src/PostgreSql/PostgreSqlLockManager.cs
@@ -18,7 +18,7 @@ public class PostgreSqlLockManager : LockManagerBase
     public sealed override async Task<bool> TryAdquireAsync(CancellationToken cancellationToken = default)
     {
         _transaction = await Context.Database.BeginTransactionAsync(IsolationLevel.ReadCommitted, cancellationToken).ConfigureAwait(false);
-        bool? lockResult = Context.Database.SqlQueryRaw<bool?>("SELECT pg_try_advisory_lock(upgradier-lock)").AsEnumerable().First();
+        bool? lockResult = Context.Database.SqlQueryRaw<bool?>("SELECT pg_try_advisory_lock(20230412)").AsEnumerable().First();
         await Context.EnsureSchema(cancellationToken).ConfigureAwait(false);
         return lockResult == true;
     }
